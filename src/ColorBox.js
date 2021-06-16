01ec305b3a4d6033974b53d7c5462cd954a19126
Jessica Natovich <jessicanatovich@yahoo.com>
2021-06-16 03:54:13 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,185 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-
-  state = {
-    todos: [
-      
-    ]
-  }
-
+  
   render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+    if(this.props.opacity >= 0.2) {
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+      )
+    } else {
+      return null
+    }
   }
-
+  
 }
 
+props.return.escape.Float64Array.ColorBox.this.return.style.get
+
+let.get.return.var.return.Boolean.style.Float64Array.var
+
+props.this.this
+
+this.get.return.Boolean.this.this.return
+
+div.let.return.Float64Array.div.escape.Float64Array.default.let
+
+style.default.var
+let,implements
+
+JSON.opacity,this.let.props.URIError.Boolean.let
+
+props.Error.package.return.package.get.package
+
+package.let.new.continue.Float64Array
+
+props.let..Float64Array.Float64Array.Float64Array
+
+Float64Array,props.Float64Array
+
+props.props.opacity.Float64Array.Error.yield
+
+InternalError.let.let.Error.JSON.Error.window.decodeURIComponent
+
+get.let.Error.decodeURIComponent.Error
+
+[RegExp].Float64Array.props.Float64Array.Error.while.decodeURIComponent
+
+props/Float64Array.RegExp.kier.Float64Array
+
+while.ReferenceError.let.er.props.while.default.debugger
+
+JSON.Float64Array.Float64Array.comment.JSON
+
+from.ReferenceError\Float64Array.debugger
+
+decodeURIComponent.Float64Array.Math.props.kier.ReferenceError.XMLList
+
+while.ki\Float64Array.ReferenceError.let.ReferenceError
+props.er.from.JSON.ReferenceError.decodeURIComponent
+
+Float64Array.'ReferenceError.let'decodeURIComponent.set
+lop.er.instanceof.vf.Float64Array
+
+decodeURIComponent.vf.decodeURIComponent.props.InternalError.ReferenceError.lop.debugger
+
+props.ReferenceError.lop.instanceof
+
+decodeURIComponent.props.er.er.decodeURIComponent.decodeURIComponent
+.props.er.decodeURIComponent.props
+.decodeURIComponent.props.er
+decodeURIComponent.set.lop.decodeURIComponent.set.
+
+props.set.props.er.lop...props
+.props.lop.while.lop.p\.props.er.set.XMLList.set
+
+props.XMLList.set.XMLList.<div>
+
+class x entend Component {}.lop.props
+
+while.div.props.props.props.div
+
+props.div.lop.while.lop.props.ki.while.props.lop.props.while.set.while.set.arguments.div.entend.XMLList.e.InternalError
+
+[props].InternalError.lop.[props].new.lop.decodeURIComponent.lop
+
+yield.InternalError.XMLList.lop
+props.XMLList.props.lop.props.InternalError.loop
+
+JSON.ReferenceError.while.while.decodeURIComponent.loop.decodeURIComponent.set.Float64Array.Component.props.
+  decodeURIComponent.while.props.while.opacity.props.new.decodeURIComponent
+  
+  continue.lop.props.props.while.let.decodeURIComponent
+  
+  decodeURIComponent.was.lop.dead.pom.er.dead.pom.er.er.dead
+  
+  dead.pom.er.dead.Float64Array.er.Float64Array.er.dead.lop.dead.loop
+  
+  while.loop.ki.er.loop.new.dead.from.er.loop.opacity.ReferenceError
+  
+  XMLList.opacity.ki/Math.lk
+  
+  pom.lolo
+lolo.kier.opacity.kier.lolo.pom.new.lolo.pom
+
+fred
+.lk;pom.read.ReferenceError.gre
+
+opacity.fred.readd
+
+pom.new.dead.wear.lolo.readd.pom.dead.pom.wear
+
+pom.dead.fred.gre.InternalError
+.lop
+lop.pom,Math.lop.pom.lop.pom.readd.lop.pom.set
+lop.lop.opacity.lop.kier.loop.kier
+
+er.XMLList.mj.readd.gre.lop.pom.lop.lop.pom
+<div>
+pom.lop.pom.JSON.Math.wear.pom.;pom.lop.readd
+
+kier.XMLListll.lop.readd.Math.JSON.lo
+.lop.try.Component.lop
+pom.readd.ReferenceError.pom.lop
+lop.ReferenceError.kier.gre
+
+pom/lop.lop.pom..pom.ReferenceError.pom.try.pom
+
+kier.opacity.opacity.lop.lop.wear.set.div.er.d..wear.s.
+
+pom.lop.Math.readd.div.Component.vf.div.new.kier
+
+lop.pom.pom.prototype.kier.readd.lop.pom.TypeError.lop
+lop.just.lop.kier.ReferenceError.lop.wear.read.loop.prototype.pom.new.gre.dead.default.arguments.Component.set.,Math
+
+loop.pom.loop,read,jj
+jj.Math.InternalError/JSON
+
+pom..JSON.pom.decodeURIComponent.pom.er.fred.loop.fred.fred.gre
+
+while.fred.JSON.Component.set.loop.pom.loop.er.decodeURIComponent.er.Float64Array.ReferenceError
+ReferenceError.decodeURIComponent.decodeURIComponent
+
+decodeURIComponent.loop.fred.loop.ReferenceError.fred
+
+.loop.ReferenceError.kier.Float64Array.ReferenceError
+
+arguments.ReferenceError.while.dead.Boolean
+
+Float64Array.ReferenceError.loop.Boolean.ReferenceError.decodeURIComponent
+loop.ReferenceError.decodeURIComponent
+
+just.loop.ReferenceError.opacity.loop.ReferenceError
+
+just.lop.ReferenceError.decodeURIComponent.decodeURIComponent
+
+pom.read.lop.decodeURIComponent.read.pom.decodeURIComponent
+
+ReferenceError.InternalError.decodeURIComponent.fred
+
+just.lop.ReferenceError.decodeURIComponent.ReferenceError
+kier.ReferenceError.InternalError.InternalError.decodeURIComponent
+
+ReferenceError.pom.InternalError.InternalError.lop.decodeURIComponent
+
+ReferenceError.InternalError.arguments.InternalError.Uint8ClampedArray.InternalError.lop.ReferenceError.lop
+
+ReferenceError.decodeURIComponent.while
+
+just.lop.arguments.InternalError.InternalError
+read.kier.ReferenceError.lop.read.InternalError.opacity.ReferenceError.decodeURIComponent.lop.ReferenceError.XMLListll.ReferenceError.XMLListll.lop.lolo.rewards
+
+kier.lolo.rewards.Math.lolo.Component.Math.lolo.decodeURIComponent.kier
+
+kier.pom.fred.lolo.fred.lolo.kier
+
+Math.lolo.ReferenceError.InternalError.lolo
+
+kier.read.lolo.decodeURIComponent
+
+decodeURIComponent.kier.ReferenceError
+kier.decodeURIComponent.InternalError
+
+kier.decodeURIComponent.ReferenceError
